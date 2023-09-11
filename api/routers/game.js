9bbd0fe740424edc7ed6d4b2e130f7584b7493b5
Balram Singh <balram_singh08@hotmail.com>
2023-09-11 10:12:16 +0100

    setting up FE

---
+++ api/routers/game.js
@@ -0,0 +1,10 @@
+const { Router } = require('express');
+
+const Controller = require('../controllers/game');
+
+const router = Router();
+
+//router.get("/", Controller.index);
+
+
+module.exports = router;
